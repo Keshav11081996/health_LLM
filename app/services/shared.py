9b553c7af37dba2c113e20b3836037d2bcd9db06
gurudt12 <101095386+gurudt12@users.noreply.github.com>
2024-09-14 22:05:11 +0530

    Update shared.py

--- app/services/shared.py
+++ app/services/shared.py
@@ -3,5 +3,5 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 
 token = os.getenv("HF_TOKEN")
 device = "cuda" if os.getenv("USE_CUDA", "true").lower() == "true" else "cpu"
-model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2").to(device)
-tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2")
+model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2", use_auth_token=token).to(device)
+tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2", use_auth_token=token)
